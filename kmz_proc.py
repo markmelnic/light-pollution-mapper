0a8e522084ec3be3570884b867b8965afff37436
Mark Melnic <markhappy200@gmail.com>
2020-11-21 13:37:53 +0100

    Added csv loader, kmz images loader

--- kmz_proc.py
+++ kmz_proc.py
@@ -1,8 +1,11 @@
 import glob, csv
+import pandas as pd
 from lxml import html
 from zipfile import ZipFile
+from PIL import Image
 
 ZIP_KML_DOC = "doc.kml"
+CSV_KML_DOC = "kml_index.csv"
 
 class KMZ:
     def __init__(self) -> None:
@@ -10,15 +13,16 @@ class KMZ:
         self.kmz_zip = ZipFile(self.kmz_file, "r")
         self.kml_file = self.kmz_zip.open(ZIP_KML_DOC, "r").read()
 
-    def indexer_csv(self,):
+    def indexer_csv(self,) -> None:
         kml_content = html.fromstring(self.kml_file)
-        with open("kml_index.csv", "w", newline="") as kml_csv:
+        with open(CSV_KML_DOC, "w", newline="") as kml_csv:
             kml_csv_writer = csv.writer(kml_csv)
             kml_csv_writer.writerow(
-                ["image", "draw_order", "north", "south", "east", "west", "rotation"]
+                ["index", "image", "draw_order", "north", "south", "east", "west", "rotation"]
             )
             for item in kml_content.cssselect("Document GroundOverlay"):
                 image = item.cssselect("name")[0].text_content()
+                index = image[23:-4]
                 draw_order = item.cssselect("drawOrder")[0].text_content()
                 coords = item.cssselect("LatLonBox")[0]
                 north = coords.cssselect("north")[0].text_content()
@@ -26,11 +30,22 @@ class KMZ:
                 east = coords.cssselect("east")[0].text_content()
                 west = coords.cssselect("west")[0].text_content()
                 rotation = coords.cssselect("rotation")[0].text_content()
-                # print([image, draw_order, north, south, east, west, rotation])
                 kml_csv_writer.writerow(
-                    [image, draw_order, north, south, east, west, rotation]
+                    [index, image, draw_order, north, south, east, west, rotation]
                 )
+
+    def load_csv(self, ) -> None:
+        self.df = pd.read_csv(CSV_KML_DOC)
+
+    def load_images(self, ) -> None:
+        self.kmz_imgs = [Image.open(self.kmz_zip.open(image)) for image in self.kmz_zip.namelist() if image.split("/")[0] == "files"]
+
+    def imager(self, ) -> None:
+        widths, heights = zip(*(img.size for img in self.kmz_imgs))
 
 if __name__ == "__main__":
     kmz = KMZ()
-    kmz.indexer_csv()
+    #kmz.indexer_csv()
+    #kmz.load_csv()
+    #kmz.imager()
+    kmz.load_images()
